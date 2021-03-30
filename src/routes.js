8cb321a1ce57906786228f9685d567eab22f5f8e
Willian Del Freu <williankeyex@gmail.com>
2021-03-30 17:58:39 -0300

    chore: 🎉 Project Finalized

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const routes = express.Router();
+
+const views = __dirname + "/views/";
+
+const profile = {
+  name: "Willian",
+  avatar: "https://avatars.githubusercontent.com/u/54725745?v=4",
+  "monthly-budget": 3000,
+  "days-per-week": 5,
+  "hours-per-day": 5,
+  "vacation-per-year": 4
+}
+
+routes.get('/', (req, res) => res.render(views + "index", { profile }));
+routes.get('/job', (req, res) => res.render(views + "job"));
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"));
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }));
+
+module.exports = routes;
